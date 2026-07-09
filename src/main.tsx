490a93b98078236a0977dc59e0c76107d2569ab5
ckojak <bmw.reta@hotmail.com>
2026-07-09 12:04:56 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+// ⚠️ ATENÇÃO: Se o seu arquivo de idioma tiver outro nome (ex: language.tsx), mude aqui na linha de baixo:
+import { LanguageProvider } from "./useLanguage.tsx"; 
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
+);
